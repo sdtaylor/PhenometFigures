fa14ce2ba5a7ade82f4381e7a666f00d0b78bde7
Shawn Taylor <shawntaylor@weecology.org>
2021-12-17 09:00:15 -0700

    download all pg data

--- get_gcc_and_phenomet_data.R
+++ get_gcc_and_phenomet_data.R
@@ -2,12 +2,9 @@ library(tidyverse)
 library(phenometR)
 library(phenocamr)
 
-phenomet_data = phenometR::get_fg_phenophase(functional_group = 'PG')
+all_pg_data = phenometR::get_fg_phenophase(functional_group = 'PG')
 
-phenomet_data = phenomet_data %>%
-  filter(SITE_CODE=='GI', SPP_CODE=='BOER', PHENOPHASE=='GR_202')
-
-write_csv(phenomet_data, './data/GI_BOER_GR202_data.csv')
+write_csv(all_pg_data, './data/PHENOMET_PG_DATA.csv')
 
 phenocamr::download_phenocam(site='ibp$', 
                              veg_type='GR',
