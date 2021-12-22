d236b27e8af181a925da2ad0460fe9b62f004bdd
Shawn Taylor <shawntaylor@weecology.org>
2021-12-22 14:08:11 -0700

    nicer colors. and save figures

--- scan_soil_moisture_figures.R
+++ scan_soil_moisture_figures.R
@@ -17,16 +17,6 @@ spei_fig = ggplot(scan_spei_and_weather_data, aes(x=month_date, y=spei)) +
         legend.position = 'none') +
   labs(y='Monthly SPEI', x='')
 
-precip_fig = ggplot(scan_spei, aes(x=month_date, y=spei)) +
-  geom_col(aes(fill=spei<0)) +
-  scale_fill_manual(values=c('blue','red')) + 
-  scale_x_date(date_breaks = '6 month', limits = as.Date(c('2009-01-01','2021-12-31'))) +
-  theme_bw() + 
-  theme(axis.text = element_text(color='black'),
-        axis.text.x = element_text(angle = -45, hjust=0),
-        legend.position = 'none') +
-  labs(y='monthly precip\nannomoly (in)', x='')
-
 #----------------------------------------------
 # soil moisture as a heatmap/timeseries
 
@@ -35,8 +25,8 @@ get_profile_soil_moisture_figure = function(this_profile=1){
     filter(profile==this_profile) %>%
   ggplot(aes(x=month_date, y=as.factor(depth), fill=avg_soil_moisture_change)) + 
     geom_tile(height=0.8, width=25) + 
-    #scale_fill_gradient2(low='red', mid='white', high='blue',midpoint=15) + 
-    scale_fill_viridis_c(option='D', na.value='white') + 
+    scale_fill_gradient2(low='firebrick', mid='lightyellow', high='darkgreen',midpoint=0, na.value='white') + 
+    #scale_fill_viridis_c(option='D', na.value='white') + 
     scale_x_date(date_breaks = '6 month', limits = as.Date(c('2009-01-01','2021-12-31'))) + 
     facet_wrap(~profile, scale='free', ncol=1, strip.position = 'right', labeller = label_both) +
     theme_bw() + 
@@ -51,7 +41,9 @@ soil_moisture_fig3 = get_profile_soil_moisture_figure(3)
 
 
 # all figures together
-spei_fig + soil_moisture_fig1 + soil_moisture_fig2 + soil_moisture_fig3 + plot_layout(ncol=1)
+main_figure1 = spei_fig + soil_moisture_fig1 + soil_moisture_fig2 + soil_moisture_fig3 + plot_layout(ncol=1)
+
+ggsave('./scan_soil_moisture_and_spei_timeseries.png', main_figure1, dpi=300, width=30, height=25, units='cm')
 
 
 #----------------------------------------------
@@ -61,9 +53,13 @@ all_data = soil_moisture_monthly_average %>%
   left_join(scan_spei_and_weather_data, by=c('year','month','month_date')) %>%
   mutate(depth = fct_rev(as_factor(depth)))
 
-ggplot(all_data, aes(x=spei, y=avg_soil_moisture_change)) + 
+main_figure2 = ggplot(all_data, aes(x=spei, y=avg_soil_moisture_change)) + 
   geom_point() +
-  facet_grid(depth~profile, label=label_both)
+  geom_hline(yintercept = 0, color='red') + 
+  facet_grid(depth~profile, label=label_both) +
+  theme_bw(15) +
+  labs(x='Monthly SPEI', y='Monthly change in soil moisture %')
 
+ggsave('./scan_soil_moisture_and_spei_scatterplot.png', main_figure2, dpi=300, width=20, height=20, units='cm')
 
 
