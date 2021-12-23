4eb4144c52d15d1ab943faf07a27932dbf6e9f48
Shawn Taylor <shawntaylor@weecology.org>
2021-12-22 17:16:02 -0700

    adjust colors and add solstice lines

--- scan_soil_moisture_figures.R
+++ scan_soil_moisture_figures.R
@@ -6,9 +6,13 @@ library(patchwork)
 scan_spei_and_weather_data = read_csv('./scan/scan_spei_and_weather.csv')
 soil_moisture_monthly_average = read_csv('./scan/scan_monthly_soil_moisture.csv')
 
+
+solstice_dates = tibble(solstice = as.Date(paste(2010:2021, '-06-21', sep = '')))
+
 #----------------------------------------------
 spei_fig = ggplot(scan_spei_and_weather_data, aes(x=month_date, y=spei)) +
   geom_col(aes(fill=spei<0)) +
+  geom_vline(data=solstice_dates, aes(xintercept=solstice), color='gold') + 
   scale_fill_manual(values=c('blue','red')) + 
   scale_x_date(date_breaks = '6 month', limits = as.Date(c('2009-01-01','2021-12-31'))) +
   theme_bw() + 
@@ -25,7 +29,8 @@ get_profile_soil_moisture_figure = function(this_profile=1){
     filter(profile==this_profile) %>%
   ggplot(aes(x=month_date, y=as.factor(depth), fill=avg_soil_moisture_change)) + 
     geom_tile(height=0.8, width=25) + 
-    scale_fill_gradient2(low='firebrick', mid='lightyellow', high='darkgreen',midpoint=0, na.value='white') + 
+    geom_vline(data=solstice_dates, aes(xintercept=solstice), color='gold') + 
+    scale_fill_gradient2(low='firebrick', mid='lightyellow', high='darkgreen',limits=c(-10,10),midpoint=0, na.value='white') + 
     #scale_fill_viridis_c(option='D', na.value='white') + 
     scale_x_date(date_breaks = '6 month', limits = as.Date(c('2009-01-01','2021-12-31'))) + 
     facet_wrap(~profile, scale='free', ncol=1, strip.position = 'right', labeller = label_both) +
