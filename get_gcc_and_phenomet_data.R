214a3450ee34c01b30f192593f2c40a670cf15ac
Shawn <shawntaylor@weecology.org>
2021-12-29 09:17:17 -0700

    download scan data

--- get_gcc_and_phenomet_data.R
+++ get_gcc_and_phenomet_data.R
@@ -2,9 +2,15 @@ library(tidyverse)
 library(phenometR)
 library(phenocamr)
 
+# data for all grasses
 all_pg_data = phenometR::get_fg_phenophase(functional_group = 'PG')
 
 write_csv(all_pg_data, './data/PHENOMET_PG_DATA.csv')
+
+# data for everything at scan
+all_scan_data = phenometR::get_site_phenophase(site_code = 'SC')
+
+write_csv(all_scan_data, './data/PHENOMET_SCAN_DATA.csv')
 
 phenocamr::download_phenocam(site='ibp$', 
                              veg_type='GR',
